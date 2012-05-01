efe3160b980483917a48725479f21a00f39e5de7
Missaoui Chedy <missaoui.chedy@gmail.com>
2012-05-01 01:47:19 +0100

    Refactored Food.h Rename Function : isGood->isInGrid Create Function : centerPosition

--- Food.h
+++ Food.h
@@ -8,8 +8,8 @@ struct FOOD {
  int size;
  SDL_Rect position;
 };
-
 typedef struct FOOD Food;
+
 Food createFood(int size){
   Food temp;
   temp.size=size;
@@ -17,31 +17,30 @@ Food createFood(int size){
   temp.position.x=300;
   temp.position.y=300; 
   return temp;
- }
+  }
 void destroyFood(Food food){
   SDL_FreeSurface(food.surface);
-}
+  }
 void displayFood(SDL_Surface *dest,Food food){
   SDL_BlitSurface(food.surface,NULL,dest,&(food.position));
-}
-int isGood(SDL_Rect position){
- if ((position.x%SEGMENT==0)&&(position.y%SEGMENT==0)){
-      return 1;
+  }
+int isInGrid(SDL_Rect position){
+ return  ((position.x%SEGMENT==0)&&(position.y%SEGMENT==0));
    }
- else {
-      return 0;
-   } 
-}
+SDL_Rect centerPosition(Food food){
+  int opt= (SEGMENT/2)-(food.size/2);
+  food.position.x+=opt;
+  food.position.y+=opt;
+  return food.position ;
+  }
 void regenFood(Food *food) {
   extern ScreenWidth;
   extern ScreenHeight;
   do {
-  food->position.x = (rand()%(ScreenWidth-SEGMENT));
-  food->position.y = (rand()%(ScreenHeight-SEGMENT));
+      food->position.x = (rand()%(ScreenWidth-SEGMENT));
+      food->position.y = (rand()%(ScreenHeight-SEGMENT));
   }
-  while (!isGood(food->position)) ; 
-  int opt= (SEGMENT/2)-(food->size/2);
-  food->position.x+=opt;
-  food->position.y+=opt;
+  while (!isInGrid(food->position)) ; 
+  food->position=centerPosition(*food);
 }
 #endif
